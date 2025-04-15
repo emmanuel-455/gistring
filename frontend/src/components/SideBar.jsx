1bfd7a2af78f5f8da7cb7329e2af0208e1c3827c
Ngene Arinzechukwu <120824375+emmanuel-455@users.noreply.github.com>
2025-04-15 12:16:31 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -48,7 +48,7 @@ const Sidebar = () => {
             key={user._id}
             onClick={() => setSelectedUser(user)}
             className={`
-              w-full p-3 flex items-center gap-3
+              w-full p-1 flex items-center gap-3
               hover:bg-base-300 transition-colors
               ${selectedUser?._id === user._id ? "bg-base-300 ring-1 ring-base-300" : ""}
             `}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
    //onsole.log(selectedUser)
   return (
     <div className="h-screen pt-[90px] bg-base-200">
-      <div className="flex items-center justify-center pt-20px px-4">
+      <div className="flex items-center justify-center pt-20px">
          <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
             <div className="flex h-full rounded-lg overflow-hidden">
                <SideBar />
