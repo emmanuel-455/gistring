cd0e145cb4f6dc057a9979aa2111252c9a3fc1e5
Ngene Arinzechukwu <120824375+emmanuel-455@users.noreply.github.com>
2025-04-15 10:41:41 +0100

    server production fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,43 @@
-import express from 'express'
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import dotenv from 'dotenv'
-import { connectDB } from './lib/db.js'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import { app, server } from './lib/socket.js'
-import path from "path"
 
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-dotenv.config()
+import path from "path";
 
-const __dirname = path.resolve()
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
-const PORT = process.env.PORT
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(cors(
-   {
-      origin: 'http://localhost:5173', // replace with your frontend url
-      credentials: true
-   }
-))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-if(process.env.NODE.ENV === "production"){
-   app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-   app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-   })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-   console.log('Server is running on port ' + PORT)
-   connectDB()
-})
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
